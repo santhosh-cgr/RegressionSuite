ca3fdac2c9963b5ddc5dc6caa0ac9f36754aae99
Santhosh <santhosh.nidamanuri@corpgovrisk.com>
2025-08-06 10:04:39 +0530

    Added implicit wait to test suite

--- Test Suites/Indicator/Indicator Test Suite.ts
+++ Test Suites/Indicator/Indicator Test Suite.ts
@@ -5,11 +5,11 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
-   <pageLoadTimeout>30</pageLoadTimeout>
-   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>true</rerunImmediately>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>3addff09-85f9-4208-9ab2-36012b5e5dcc</testSuiteGuid>
    <testCaseLink>
       <guid>7b97cee4-7ce2-4086-bda4-2072e937ecfe</guid>
